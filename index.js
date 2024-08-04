eebf87d9df725e2fbe4e0550dafbcc923a60b241
TusharMinche <tusharminche@gmail.com>
2024-08-04 17:22:04 +0530

    update for cros origin

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ database.connect(); // Connect to the database
 
 app.use(express.json()); // Middleware to parse JSON requests
 app.use(cookieParser()); // Middleware to parse cookies
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "https://frontend-studynotion.onrender.com/", // Allow requests from this origin
+    credentials: true, // Enable credentials (cookies, authorization headers)
+  })
+);
 
 app.use(
   fileUpload({
